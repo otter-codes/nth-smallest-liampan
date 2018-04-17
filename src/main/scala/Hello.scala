4747dfb760f02c535ae58cd659062d4f545b1861
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-04-17 14:23:52 +0100

    fixed bug where list was not distinct - where if a list containing a duplicate could still call an out of bounds number

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,7 +2,7 @@ object Hello extends App {
 
   def apply(listOfNumbers : List[Int], nthTerm : Int) : Int  = {
     if (listOfNumbers == Nil) {throw new Exception("no numbers to choose from")}
-    if (listOfNumbers.size < nthTerm) {throw new Exception("index not allowed")}
+    if (listOfNumbers.distinct.size < nthTerm) {throw new Exception("index not allowed")}
     listOfNumbers.distinct.sorted.reverse(nthTerm-1)
   }
 }
