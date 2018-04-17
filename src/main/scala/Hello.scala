a2890a09749b180ead5442bece8e1778873ed6f0
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-04-17 13:10:12 +0100

    All tests passing, with disticted and sorted list, exceptions for invalid inputs. pan

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,8 @@
 object Hello extends App {
 
-  def greet : String = "hello"
-
+  def apply(listOfNumbers : List[Int], nthTerm : Int) : Int  = {
+    if (listOfNumbers == Nil) {throw new Exception("no numbers to choose from")}
+    if (listOfNumbers.size < nthTerm) {throw new Exception("index not allowed")}
+    listOfNumbers.distinct.sorted.reverse(nthTerm-1)
+  }
 }
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,13 +1,59 @@
 import org.scalatest._
 
+import scala.util.Random
+
 class HelloSpec extends WordSpec with MustMatchers {
 
-  "Hello" must {
+  "apply" must {
 
-    "say hello" in {
-      Hello.greet mustEqual "hello"
+
+
+    "return Exception if list is empty with n=1 " in {
+      val Ex = intercept[Exception]{Hello.apply(List(),1)}
+
+      assert(Ex.getMessage == "no numbers to choose from")
+    }
+
+    "return head of list(1) if List(1) n = 1 " in {
+
+      Hello.apply(List(1),1) mustEqual 1
+    }
+
+
+
+    val x = Random.nextInt(100)
+    s"return head of list(x) if List(x) n = 1 where x= $x " in  {
+
+      Hello.apply(List(x),1) mustEqual x
+    }
+
+
+    "return 10 When given List(10,9) n = 1" in {
+
+      Hello.apply(List(10,9),1) mustEqual 10
+    }
+
+    "return exception if index extends list" in {
+      val Ex = intercept[Exception]{Hello.apply(List(1,2,3),4)}
+
+      assert(Ex.getMessage == "index not allowed")
+    }
+
+    "return 10 when given List(9,10) n = 1" in {
+
+      Hello.apply(List(9,10),1) mustEqual 10
+    }
+
+    "return 10 when given List(12,11,11,10) n = 3 " in {
+
+      Hello.apply(List(12,11,11,10),3) mustEqual 10
+    }
+
+
+    "return 10 from a large unordered list containing duplicates, when 10 is the nth(3rd) largest" in {
+
+      Hello.apply(List(10,10,12,13,3,2,2,5),3) mustEqual 10
     }
 
   }
-
 }
